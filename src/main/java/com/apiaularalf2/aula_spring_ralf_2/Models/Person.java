4798031ec8ef5b287c69398be8646ac15a595388
Thimachal <thimachal@gmail.com>
2023-11-02 12:00:54 -0300

    models

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Models/Person.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Models/Person.java
@@ -1,4 +1,56 @@
 package com.apiaularalf2.aula_spring_ralf_2.Models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private EnumGender gender;
+    private Integer age;
+
+    public Person(){
+
+    }
+    public Person(Long id, String name, EnumGender gender, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EnumGender getGender() {
+        return gender;
+    }
+
+    public void setGender(EnumGender gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
