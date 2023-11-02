82ac7e706606a774d35c45b0172bb097948f2c1f
Thimachal <thimachal@gmail.com>
2023-11-02 11:49:52 -0300

    models

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ProductsController.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ProductsController.java
@@ -1,8 +1,13 @@
 package com.apiaularalf2.aula_spring_ralf_2.Controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/products")
+@RestController
 public class ProductsController {
-    
+    @GetMapping("/")
+    public String messenger(){
+        return "hello";
+    }
 }
