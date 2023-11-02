f150a1ae2ce65fb6da4c88e2f0b30288f6ac7a73
Thimachal <thimachal@gmail.com>
2023-11-02 14:35:14 -0300

    models

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/PersonController.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/PersonController.java
@@ -1,11 +1,10 @@
 package com.apiaularalf2.aula_spring_ralf_2.Controllers;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProductsController {
+public class PersonController {
     @GetMapping("/person")
     public String messenger(){
         return "hello";
