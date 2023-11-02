31d47b4ac274f1e82b3eaf50fa99d711d10215bf
Thimachal <thimachal@gmail.com>
2023-11-02 20:00:38 -0300

    jpa

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Repositories/PersonRepository.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Repositories/PersonRepository.java
@@ -1,4 +1,7 @@
 package com.apiaularalf2.aula_spring_ralf_2.Repositories;
 
-public interface PersonRepository {
+import com.apiaularalf2.aula_spring_ralf_2.Models.Person;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonRepository extends CrudRepository<Person, Long> {
 }
