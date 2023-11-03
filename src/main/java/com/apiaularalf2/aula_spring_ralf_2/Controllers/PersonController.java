06da941068e636b89d7353087f0ee3dcc8974daf
Thimachal <thimachal@gmail.com>
2023-11-03 17:32:54 -0300

    jpa

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/PersonController.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/PersonController.java
@@ -3,12 +3,10 @@ package com.apiaularalf2.aula_spring_ralf_2.Controllers;
 import com.apiaularalf2.aula_spring_ralf_2.Models.Person;
 import com.apiaularalf2.aula_spring_ralf_2.Repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PersonController {
@@ -18,6 +16,11 @@ public class PersonController {
     @GetMapping("/persons")
     public List<Person> listAllPersons(){
         return action.findAll();
+    }
+
+    @GetMapping("/persons/{idFromUrl}")
+    public Optional<Person> listById(@PathVariable Long idFromUrl){
+        return action.findById(idFromUrl);
     }
 
     @PostMapping("/personsregister")
