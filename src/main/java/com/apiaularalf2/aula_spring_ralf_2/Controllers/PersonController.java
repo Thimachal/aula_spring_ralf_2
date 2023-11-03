4221cb6b0e9cbb988e01e7de61160fbb7da2c29a
Thimachal <thimachal@gmail.com>
2023-11-03 17:15:16 -0300

    jpa

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/PersonController.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/PersonController.java
@@ -1,12 +1,28 @@
 package com.apiaularalf2.aula_spring_ralf_2.Controllers;
 
+import com.apiaularalf2.aula_spring_ralf_2.Models.Person;
+import com.apiaularalf2.aula_spring_ralf_2.Repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PersonController {
-    @GetMapping("/person")
-    public String messenger(){
-        return "hello";
+
+    @Autowired
+    private PersonRepository action;
+    @GetMapping("/persons")
+    public List<Person> listAllPersons(){
+        return action.findAll();
+    }
+
+    @PostMapping("/personsregister")
+    public Person register(@RequestBody Person personFromFront){
+
+        return action.save(personFromFront);
     }
 }
