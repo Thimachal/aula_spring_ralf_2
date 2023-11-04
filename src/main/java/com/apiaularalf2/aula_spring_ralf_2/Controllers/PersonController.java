e2f1f1c4ef2008a142c762da68c5b2cbe4486cbd
Thimachal <thimachal@gmail.com>
2023-11-04 14:53:39 -0300

    service

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/PersonController.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/PersonController.java
@@ -3,46 +3,40 @@ package com.apiaularalf2.aula_spring_ralf_2.Controllers;
 import com.apiaularalf2.aula_spring_ralf_2.Models.Person;
 import com.apiaularalf2.aula_spring_ralf_2.Services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class PersonController {
 
     @Autowired
-    private PersonService actionServ;
+    private PersonService actionService;
 
+    //Rota para cadastrar pessoas
     @PostMapping("/personsregister")
     public ResponseEntity<?> register(@RequestBody Person personFromFront){
 
-        return actionServ.register(personFromFront);
+        return actionService.registerPersons(personFromFront);
     }
 
-
-
-    @GetMapping("persons/status")
-    public ResponseEntity<?> status(){
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    //Rota para listas todas as pessoas cadastradas
+    @GetMapping("/persons")
+    public ResponseEntity<?> list(){
+        return actionService.listAllPersons();
     }
 
+    //Rota para selecionar uma pessoa pelo seu código cadastrado
+    @GetMapping("/persons/{idFromUrl}")
+    public ResponseEntity<?> listPersonsById(@PathVariable Long idFromUrl){
+        return actionService.selectPersonById(idFromUrl);
+
+    }
 
 
 /*
-    //Rotas sem Response entity
-    @GetMapping("/persons")
-    public List<Person> listAllPersons(){
-        return action.findAll();
-    }
 
-    @GetMapping("/persons/{idFromUrl}")
-    public Optional<Person> listPersonsById(@PathVariable Long idFromUrl){
-        return action.findById(idFromUrl);
 
-    }
 
    @DeleteMapping("/persons/{idFromUrl}")
     public void deletPersonById(@PathVariable Long idFromUrl) {
