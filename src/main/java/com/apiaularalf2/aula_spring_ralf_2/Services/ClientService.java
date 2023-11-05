9e3c09e748ac7e7f9a11648252847773105cd09a
Thimachal <thimachal@gmail.com>
2023-11-05 12:00:01 -0300

    validation

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Services/ClientService.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Services/ClientService.java
@@ -2,10 +2,15 @@ package com.apiaularalf2.aula_spring_ralf_2.Services;
 
 import com.apiaularalf2.aula_spring_ralf_2.Repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
     @Autowired
     private ClientRepository actionRepository;
+
+    public ResponseEntity<?> listAllClients() {
+        return new ResponseEntity<>(actionRepository.findAll());
+    }
 }
