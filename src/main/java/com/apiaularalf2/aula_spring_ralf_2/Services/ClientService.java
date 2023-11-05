758db8cf5ad23533b00d446d2b4cd8118706c4a3
Thimachal <thimachal@gmail.com>
2023-11-05 11:57:05 -0300

    validation

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Services/ClientService.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Services/ClientService.java
@@ -1,7 +1,11 @@
 package com.apiaularalf2.aula_spring_ralf_2.Services;
 
+import com.apiaularalf2.aula_spring_ralf_2.Repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
+    @Autowired
+    private ClientRepository actionRepository;
 }
