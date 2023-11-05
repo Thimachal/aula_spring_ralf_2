26e35d8a4f40cf8a9e318185bfc20c2428f3b560
Thimachal <thimachal@gmail.com>
2023-11-05 11:56:25 -0300

    validation

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ClientController.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ClientController.java
@@ -3,10 +3,14 @@ package com.apiaularalf2.aula_spring_ralf_2.Controllers;
 import com.apiaularalf2.aula_spring_ralf_2.Repositories.ClientRepository;
 import com.apiaularalf2.aula_spring_ralf_2.Services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
     @Autowired
     private ClientService actionService;
+    @GetMapping("/client")
+    public ResponseEntity<?>
 }
