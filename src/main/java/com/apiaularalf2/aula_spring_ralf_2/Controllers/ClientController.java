cb9226f7692806a0ad514c2d034725fa495076aa
Thimachal <thimachal@gmail.com>
2023-11-05 12:13:31 -0300

    validation

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ClientController.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ClientController.java
@@ -1,17 +1,27 @@
 package com.apiaularalf2.aula_spring_ralf_2.Controllers;
 
-import com.apiaularalf2.aula_spring_ralf_2.Repositories.ClientRepository;
+import com.apiaularalf2.aula_spring_ralf_2.Models.Client;
 import com.apiaularalf2.aula_spring_ralf_2.Services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClientController {
     @Autowired
     private ClientService actionService;
-    @GetMapping("/client")
+
+    @PostMapping("/clientsregister")
+    public ResponseEntity<?> register(@RequestBody Client clientFromFront){
+        return actionService.registerClients(clientFromFront);
+    }
+
+
+
+    @GetMapping("/clients")
     public ResponseEntity<?> list(){
         return actionService.listAllClients();
     }
