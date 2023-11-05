bfd1e821698eaf66f454f67c2fdac80cb1dc56d8
Thimachal <thimachal@gmail.com>
2023-11-05 12:22:54 -0300

    validation

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ClientController.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ClientController.java
@@ -2,6 +2,7 @@ package com.apiaularalf2.aula_spring_ralf_2.Controllers;
 
 import com.apiaularalf2.aula_spring_ralf_2.Models.Client;
 import com.apiaularalf2.aula_spring_ralf_2.Services.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ public class ClientController {
     private ClientService actionService;
 
     @PostMapping("/clientsregister")
-    public ResponseEntity<?> register(@RequestBody Client clientFromFront){
+    public ResponseEntity<?> register(@Valid @RequestBody Client clientFromFront){
         return actionService.registerClients(clientFromFront);
     }
 
