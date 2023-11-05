9d86f689bb14df8a989d989734e5cc2296db7e45
Thimachal <thimachal@gmail.com>
2023-11-05 12:13:55 -0300

    validation

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Services/ClientService.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Services/ClientService.java
@@ -1,7 +1,9 @@
 package com.apiaularalf2.aula_spring_ralf_2.Services;
 
+import com.apiaularalf2.aula_spring_ralf_2.Models.Client;
 import com.apiaularalf2.aula_spring_ralf_2.Repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,11 @@ public class ClientService {
     private ClientRepository actionRepository;
 
     public ResponseEntity<?> listAllClients() {
-        return new ResponseEntity<>(actionRepository.findAll());
+        return new ResponseEntity<>(actionRepository.findAll(), HttpStatus.OK);
+    }
+
+
+    public ResponseEntity<?> registerClients(Client clientObj) {
+        return new ResponseEntity<>(actionRepository.save(clientObj), HttpStatus.OK);
     }
 }
