92a97f4415bac0b10c093cada19305094e27c921
Thimachal <thimachal@gmail.com>
2023-11-05 11:45:34 -0300

    validation

---
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Models/Client.java
@@ -0,0 +1,41 @@
+package com.apiaularalf2.aula_spring_ralf_2.Models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+@Entity
+@Table
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idClient;
+    @NotEmpty(message = "Informe um nome")
+    private String nameClient;
+    @Email(message = "Informe um email válido")
+    private String emailClient;
+
+    public Long getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(Long idClient) {
+        this.idClient = idClient;
+    }
+
+    public String getNameClient() {
+        return nameClient;
+    }
+
+    public void setNameClient(String nameClient) {
+        this.nameClient = nameClient;
+    }
+
+    public String getEmailClient() {
+        return emailClient;
+    }
+
+    public void setEmailClient(String emailClient) {
+        this.emailClient = emailClient;
+    }
+}
