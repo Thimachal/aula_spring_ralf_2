5e88aa58d82646ff10ed16e814ecc3ae8cef069d
Thimachal <thimachal@gmail.com>
2023-11-10 20:57:24 -0300

    validation

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Services/ClientService.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Services/ClientService.java
@@ -1,6 +1,7 @@
 package com.apiaularalf2.aula_spring_ralf_2.Services;
 
 import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
+import com.apiaularalf2.aula_spring_ralf_2.Models.Client;
 import com.apiaularalf2.aula_spring_ralf_2.Repositories.ClientRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,17 @@ public class ClientService {
 
     public ResponseEntity<?> registerClients(@Valid ClientDTO clientObj) {
         return new ResponseEntity<>(actionRepository.save(clientObj), HttpStatus.OK);
+    }
+
+    public static Client builderDTOModel(ClientDTO clientFromFront) {
+        var client = new Client();
+
+        //opcao para transformar todos os itens de client para DTO
+        client.setIdClient(clientFromFront.getIdClientDTO());
+        client.setNameClient(clientFromFront.getNameClientDTO());
+        client.setEmailClient(client.getEmailClient());
+
+
+        return client;
     }
 }
