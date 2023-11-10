ca32030ef3498b10fcd5282b6d80bf31ca195d80
Thimachal <thimachal@gmail.com>
2023-11-10 20:56:34 -0300

    validation

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Repositories/ClientRepository.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Repositories/ClientRepository.java
@@ -1,6 +1,8 @@
 package com.apiaularalf2.aula_spring_ralf_2.Repositories;
 
 import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
+import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
+import com.apiaularalf2.aula_spring_ralf_2.Services.ClientService;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
