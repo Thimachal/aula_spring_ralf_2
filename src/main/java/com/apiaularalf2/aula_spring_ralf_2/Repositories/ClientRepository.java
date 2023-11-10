b7d106a3adfd665b21fe7556a22a6a7e902b77b6
Thimachal <thimachal@gmail.com>
2023-11-09 22:43:20 -0300

    validation

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Repositories/ClientRepository.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Repositories/ClientRepository.java
@@ -1,10 +1,10 @@
 package com.apiaularalf2.aula_spring_ralf_2.Repositories;
 
-import com.apiaularalf2.aula_spring_ralf_2.Models.Client;
+import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface ClientRepository extends CrudRepository<Client, Long> {
-    List<Client> findAll();
+public interface ClientRepository extends CrudRepository<ClientDTO, Long> {
+    List<ClientDTO> findAll();
 }
