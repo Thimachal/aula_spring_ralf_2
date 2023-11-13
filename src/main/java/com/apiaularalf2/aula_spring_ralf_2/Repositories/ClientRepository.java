a19ffd411709133464907d751632f045526c8f8f
Thimachal <thimachal@gmail.com>
2023-11-13 18:25:40 -0300

    DTO 2.0

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Repositories/ClientRepository.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Repositories/ClientRepository.java
@@ -1,12 +1,11 @@
 package com.apiaularalf2.aula_spring_ralf_2.Repositories;
 
-import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
-import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
-import com.apiaularalf2.aula_spring_ralf_2.Services.ClientService;
+
+import com.apiaularalf2.aula_spring_ralf_2.Models.Client;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface ClientRepository extends CrudRepository<ClientDTO, Long> {
-    List<ClientDTO> findAll();
+public interface ClientRepository extends CrudRepository<Client, Long> {
+    List<Client> findAll();
 }
