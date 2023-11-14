0022fc012afcf355ddc93c9b8c85f74abf2a7508
Thimachal <thimachal@gmail.com>
2023-11-13 21:24:30 -0300

    DTO 2.0

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ClientController.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Controllers/ClientController.java
@@ -1,10 +1,10 @@
 package com.apiaularalf2.aula_spring_ralf_2.Controllers;
 
-import com.apiaularalf2.aula_spring_ralf_2.Models.Client;
 import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
 import com.apiaularalf2.aula_spring_ralf_2.Services.ClientService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +18,13 @@ public class ClientController {
 
     @PostMapping("/clientsregister")
     public ResponseEntity<?> register(@Valid @RequestBody ClientDTO clientFromFront){
-        var client = actionService.builderDTOModel(clientFromFront);
-        return actionService.registerClients(client);
+       ResponseEntity<?> clientFromService = actionService.builderDTOModel(clientFromFront);
+        if (clientFromService != null) {
+            return new ResponseEntity<>(clientFromService.getBody(),HttpStatus.CREATED);
+        }else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
     }
     @GetMapping("/clients")
     public ResponseEntity<?> list(){
