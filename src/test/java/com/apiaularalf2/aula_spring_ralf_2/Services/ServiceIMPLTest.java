dd89f009dd7c424982a6585f12f0ef1b04834c0b
Thimachal <thimachal@gmail.com>
2023-11-14 09:11:42 -0300

    impl teste

--- src/test/java/com/apiaularalf2/aula_spring_ralf_2/Services/ServiceIMPLTest.java
+++ src/test/java/com/apiaularalf2/aula_spring_ralf_2/Services/ServiceIMPLTest.java
@@ -1,5 +1,7 @@
 package com.apiaularalf2.aula_spring_ralf_2.Services;
 
+import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
+import com.apiaularalf2.aula_spring_ralf_2.Models.Client;
 import com.apiaularalf2.aula_spring_ralf_2.Services.IMPL.ClientInterface;
 import com.apiaularalf2.aula_spring_ralf_2.Services.IMPL.ClientService;
 import org.junit.jupiter.api.Test;
@@ -12,17 +14,18 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class ServiceIMPLTest {
-    public static final Integer ID = 20;
+    public static final Long ID = 20L;
     public static final String NAME = "JOSEFINA";
     public static final String EMAIL = "TESTANDOEMAIL";
     @InjectMocks
     private ClientService client;
     @Mock
     private ClientInterface clientRepository;
+    Client test = new Client(ID,NAME,EMAIL);
 
     @Test
     void TestCriation(){
-        when(clientRepository.registerClientDTO(any())).thenReturn();
+        when(clientRepository.registerClientDTO(any())).thenReturn(test);
     }
 
 }
