a5a2e190dd462fd67d2879ab45cb737294f90e57
Thimachal <thimachal@gmail.com>
2023-11-14 08:52:46 -0300

    impl teste

--- src/main/java/com/apiaularalf2/aula_spring_ralf_2/Models/Client.java
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Models/Client.java
@@ -1,7 +1,5 @@
 package com.apiaularalf2.aula_spring_ralf_2.Models;
 
-import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
-import com.apiaularalf2.aula_spring_ralf_2.Services.ClientService;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
