756d0cb8df7b95652811e4ec9e3a363df4f11e5a
Thimachal <thimachal@gmail.com>
2023-11-14 08:53:09 -0300

    impl teste

---
+++ src/main/java/com/apiaularalf2/aula_spring_ralf_2/Services/IMPL/ClientInterface.java
@@ -0,0 +1,8 @@
+package com.apiaularalf2.aula_spring_ralf_2.Services.IMPL;
+
+import com.apiaularalf2.aula_spring_ralf_2.DTOs.ClientDTO;
+import org.springframework.http.ResponseEntity;
+
+public interface ClientInterface {
+    public ResponseEntity<?> registerClientDTO(ClientDTO clientFromFront);
+}
